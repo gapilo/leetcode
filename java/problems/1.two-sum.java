523176abbeab8b08a30f1e419b33d3017a1b0b75
Milos Copic <miloscopic95@gmail.com>
2025-11-03 18:56:36 -0500

    Two Sum solution

--- java/problems/1.two-sum.java
+++ java/problems/1.two-sum.java
@@ -1,12 +1,22 @@
-// @leet imports start
 import java.util.*;
-import java.math.*;
-// @leet imports end
 
+// @leet imports end
 // @leet start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
+      Map<Integer, Integer> map = new HashMap<>();
+      
+      for (int i = 0; i < nums.length; i++) {
+        map.put(nums[i], i);
+      }
+
+      for (int i = 0; i < nums.length; i++) {
+        int targetNum = target - nums[i];
+        if (map.containsKey(targetNum) && map.get(targetNum) != i) {
+          return new int[] {i, map.get(targetNum)};
+        }
+    }
+       return new int[]{}; 
     }
 }
 // @leet end
