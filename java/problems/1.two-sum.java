a95ddf23b6186fe2260385540f24f38c21bf6348
Milos Copic <miloscopic95@gmail.com>
2025-11-02 21:30:43 -0500

    Setting up leetcode nvim plugin

---
+++ java/problems/1.two-sum.java
@@ -0,0 +1,12 @@
+// @leet imports start
+import java.util.*;
+import java.math.*;
+// @leet imports end
+
+// @leet start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+    }
+}
+// @leet end
